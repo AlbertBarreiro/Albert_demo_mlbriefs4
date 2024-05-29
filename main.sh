daec6e2e3485481ef45a3604743581753fe77fad
AlbertBarreiro <albertbarreirod@gmail.com>
2024-05-29 16:07:10 +0200

    yes

--- main.sh
+++ main.sh
@@ -2,10 +2,11 @@ dataset=$(cat $input_0)
 img=$2
  
 img_nb=$(printf "%03d" "$img")
+img_nb4=$(printf "%04d" "$img")
  
 cp $bin/models/refnerf/"$dataset"/rgb/"$img_nb".png image0.png
 cp $bin/models/nrff/"$dataset"/rgb/"$img_nb".png image1.png
-cp $bin/models/gt/"$dataset"/rgb/"$img_nb".png image2.png
+cp $bin/models/gt/"$dataset"/rgb/"$img_nb4".png image2.png
 
 
 python $bin/metrics.py image0.png image2.png ./metrics_refnerf
