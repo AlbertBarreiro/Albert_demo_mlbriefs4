104ac07f34315e95ccc5cabbc0a07baed84a8f55
AlbertBarreiro <albertbarreirod@gmail.com>
2024-05-29 14:14:32 +0200

    prueba

--- main.sh
+++ main.sh
@@ -4,6 +4,5 @@ img_nb=$(printf "%03d" "$img")
  
 cp $bin/models/refnerf/"$dataset"/rgb/"$img_nb".png image0.png
 cp $bin/models/nrff/"$dataset"/rgb/"$img_nb".png image1.png
-cp $bin/models/nrff/"$dataset"/rgb/"$img_nb".png gt.png
 
-python $bin/metrics.py image0.png gt.png ./metrics
+python $bin/metrics.py image0.png image1.png ./metrics
