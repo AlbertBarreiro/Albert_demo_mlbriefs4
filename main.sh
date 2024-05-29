03c4a835322ebebea3d7d30fe2651c2e87c4a511
AlbertBarreiro <albertbarreirod@gmail.com>
2024-05-29 14:46:27 +0200

    .

--- main.sh
+++ main.sh
@@ -6,3 +6,4 @@ img_nb=$(printf "%03d" "$img")
 cp $bin/models/refnerf/"$dataset"/rgb/"$img_nb".png image0.png
 cp $bin/models/nrff/"$dataset"/rgb/"$img_nb".png image1.png
 
+python $bin/metrics.py image0.png image1.png ./metrics
