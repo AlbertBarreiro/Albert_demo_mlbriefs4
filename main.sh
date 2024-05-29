42b575e9cd331070a86ef6c7e150e35701a8f4a1
AlbertBarreiro <albertbarreirod@gmail.com>
2024-05-29 17:57:35 +0200

    .

--- main.sh
+++ main.sh
@@ -8,7 +8,18 @@ cp $bin/models/refnerf/"$dataset"/rgb/"$img_nb".png image0.png
 cp $bin/models/nrff/"$dataset"/rgb/"$img_nb".png image1.png
 
 cp $bin/models/gt/"$dataset"/rgb/"$img_nb4".png image2.png
-convert image2.png -resize 50% image3.png
+
+# Get the original dimensions
+width=$(identify -format "%w" image2.png)
+height=$(identify -format "%h" image2.png)
+
+# Calculate new dimensions
+new_width=$((width / 2))
+new_height=$((height / 2))
+
+# Downscale the image to exact dimensions
+convert image2.png -resize "${new_width}x${new_height}" image2.png
+
 
 cp $bin/models/refnerf/"$dataset"/normals/"$img_nb".png normals_refnerf.png
 cp $bin/models/nrff/"$dataset"/normals/"$img_nb".png normals_nrff.png
